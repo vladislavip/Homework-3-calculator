7e5bb10e9e75cce9e7e7739b8400f0aaa65048bb
vladyslavip <vladyslavip@code.edu.az>
2021-12-11 13:04:25 +0300

    first commit

---
+++ turbo.az/Cars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace turbo.az
+{
+    internal class Cars
+    {
+        public string marka;
+        public string model;
+        public int year;
+        public double price;
+
+        public string GetFullName()
+        {
+            return $"{marka} {model} {year} {price}";
+        }
+
+    }
+}
---
+++ turbo.az/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace turbo.az
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type the number of cars you wish to add");
+
+            int numofcars = Convert.ToInt32(Console.ReadLine());
+
+            Cars[] carlist = new Cars[numofcars];
+
+            for (int i = 0; i < numofcars; i++)
+            {
+
+                Cars car1 = new Cars();
+
+                Console.WriteLine("Input  marka");
+                car1.marka = Console.ReadLine();
+                Console.WriteLine($"marka = {car1.marka}");
+
+
+                Console.WriteLine("Input model");
+                car1.model = Console.ReadLine();
+                Console.WriteLine($"marka = {car1.model}");
+
+                Console.WriteLine("Input year");
+                car1.year = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"year = {car1.marka}");
+
+                Console.WriteLine("Input price");
+                car1.price = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"marka = {car1.marka}");
+
+                carlist[i] = car1;
+
+
+
+            }
+
+            foreach (var Cars in carlist)
+
+
+            {
+                string name = Cars.GetFullName();
+                Console.WriteLine(name);
+
+
+            }
+
+
+
+
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
